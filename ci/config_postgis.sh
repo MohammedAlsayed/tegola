306c72fde8a8e35eccdf501befa8f76c3d4f12a1
Jivan Amara <Development@JivanAmara.net>
2018-05-01 18:35:23 -0600

    Switch from s3 to github.com/go-spatial/tegola-testdata for PostGIS data.

--- ci/config_postgis.sh
+++ ci/config_postgis.sh
@@ -2,14 +2,15 @@
 
 set -ex
 
-#	fetch our test data and import it into Postgres. 
+#	fetch our test data and import it into Postgres.
 #	this command uses pg_restore and therefore leverages the environment variables document at https://www.postgresql.org/docs/9.2/static/libpq-envars.html
 configure_postgis() {
-    local test_data="tegola.backup"
-    local test_data_url="https://s3-us-west-1.amazonaws.com/tegola-test-data/tegola-postgis-test-data.backup"
+    local test_data_url="https://raw.githubusercontent.com/go-spatial/tegola-testdata/master/tegola-postgis-test-data.backup"
+    local test_data="tegola-postgis-test-data.backup"
 
     #   fetch our test data
     curl $test_data_url > $test_data
+
 
     #   import the data to postgres
     pg_restore -C -d postgres $test_data
